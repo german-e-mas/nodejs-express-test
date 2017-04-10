be11c479a627c147e6ca72e920d7bac57c1f4f09
german-e-mas <german.e.mas@gmail.com>
2017-04-10 12:48:27 -0300

    CRUD Update and Delete

--- server.js
+++ server.js
@@ -20,13 +20,26 @@ MongoClient.connect('mongodb://localhost:27017/quotes', function(err, database) 
   })
 })
 
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
   db.collection('quotes').find().toArray(function(err, results) {
   	if (err) return console.log(err)
   	res.render('index.ejs', {quotes: results})
   })
+})
+
+app.put('/quotes', function (req, res) {
+  db.collection('quotes').findOneAndUpdate(
+    { name: 'Gero' },
+    { $set: { name: req.body.name, quote: req.body.quote } },
+    { sort: { _id: -1 }, upsert: true },
+    function (err, result) {
+      if (err) return res.send(err)
+      res.send(result)
+    })
 })
 
 app.post('/quotes', function (req, res) {
@@ -37,4 +50,13 @@ app.post('/quotes', function (req, res) {
   	console.log("Saved to DB")
     res.redirect('/')
   })
+})
+
+app.delete('/quotes', function (req, res) {
+  db.collection('quotes').findOneAndDelete(
+    { name: req.body.name },
+    function (err, result) {
+      if (err) return res.send(500, err)
+      res.json('Quote deleted.')
+    })
 })
